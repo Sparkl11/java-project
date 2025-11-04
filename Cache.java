5d78011628fcf81fee95d5eb60785d5c0345a3d6
admin <admin@localhost.localdomain>
2025-11-05 02:55:15 +0700

    создали дженерик кэш с конструктором

---
+++ Cache.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class Cache<T> {
+    private final LinkedList<T> items;
+    private final int maxSize;
+    
+    public Cache(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("Max size must be positive");
+        }
+        this.maxSize = maxSize;
+        this.items = new LinkedList<>();
+    }
+}
